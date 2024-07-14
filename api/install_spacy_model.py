b7c477b21965c4f74e10f3dfdbd24e0cad55f08c
abhitej-bokka <abhitej.bokka@gmail.com>
2024-07-13 21:53:20 -0400

    try this
    here

--- api/install_spacy_model.py
+++ api/install_spacy_model.py
@@ -1,7 +1,4 @@
-import subprocess
+import spacy
 
-def download_spacy_model():
-    subprocess.run(["python3", "-m", "spacy", "download", "en_core_web_sm"])
-
-if __name__ == "__main__":
-    download_spacy_model()
+# Download the spaCy model
+spacy.cli.download("en_core_web_sm")
